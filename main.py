6c13bc1dee441f1cff5aa4c8c2d59d32c3ce1f33
yasu91dev <yasu91dev@gmail.com>
2019-11-29 00:24:43 +0900

    Supported touch/ls commands.

--- main.py
+++ main.py
@@ -5,23 +5,62 @@ class InputString:
         self.inStream = input("$ ")
         return
 
-    def getCmd(self) -> str:
+    def getCmd(self):
         return self.inStream.split()[0]
 
-    def getArg(self):
+    def getArgNum(self):
+        return len(self.inStream.split())
+
+    def getConcatedArg(self):
         return " ".join(self.inStream.split()[1:])
 
+    def getArg(self, posi):
+        return self.inStream.split()[posi]
+
+class FileSystem:
+    fileList = []
+
+    def searchFile(self, name):
+        if( len(self.fileList) == 0 ):
+            return False
+        else:
+            for n in self.fileList:
+                if(n==name): return True
+                else:        return False
+    
+    def addFile(self, filename):
+        self.fileList.append(filename)
+        return
+
+    def showFiles(self):
+        if(len(self.fileList)!=0):
+            sortList = sorted(self.fileList)
+            print("  ".join(sortList))
+        return
+
 class MyShell:
+    fs = FileSystem()
+    
     def runShell(self):
         istr = InputString()
         
-        while 1:
+        while True:
             istr.getInput()
             cmd = istr.getCmd()
             
             if cmd == "echo":
-                print(istr.getArg())
+                print(istr.getConcatedArg())
             
+            elif cmd == "touch":
+                i = 1
+                while i < istr.getArgNum():
+                    if(self.fs.searchFile(istr.getArg(i)) == False):
+                        self.fs.addFile(istr.getArg(i))
+                    i += 1
+
+            elif cmd == "ls":
+                self.fs.showFiles()
+
             elif cmd == "exit":
                 break
             
