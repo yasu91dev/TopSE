54c0f333cd703de771ba82e982d073f8e1bafb8b
yasu91dev <yasu91dev@gmail.com>
2019-12-01 15:56:07 +0900

    Make prototype for mkdir.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 class InputString:
-    inStream = ""
+    def __init__(self):
+        self.inStream = ""
 
     def getInput(self):
         self.inStream = input("$ ")
@@ -17,29 +18,65 @@ class InputString:
     def getArg(self, posi):
         return self.inStream.split()[posi]
 
-class FileSystem:
-    fileList = []
 
-    def searchFile(self, name):
+class File:
+    def __init__(self, fileName, fileType, filePath):
+        self.fileName = fileName
+        self.fileType = fileType    # f:File, d:Directory
+        self.filePath = filePath
+
+    def getFileName(self):
+        return self.fileName
+
+    def getFileType(self):
+        return self.fileType
+    
+    def getFilePath(self):
+        return self.filePath
+
+
+class FileSystem:
+    def __init__(self):
+        self.fileList = []
+
+    def searchFile(self, fileName):
         if( len(self.fileList) == 0 ):
             return False
         else:
             for n in self.fileList:
-                if(n==name): return True
-                else:        return False
+                if(n.getFileName() == fileName):
+                    return True
+                else:
+                    return False
     
-    def addFile(self, filename):
-        self.fileList.append(filename)
+    def addFile(self, fileName, fileType, filePath):
+        if "/" in fileName:    # ファイルシステムで使用不可能な文字がfileNameに含まれている場合
+            print("")
+        else:
+            file = File(fileName, fileType, filePath)
+            self.fileList.append(file)
         return
 
     def showFiles(self):
-        if(len(self.fileList)!=0):
-            sortList = sorted(self.fileList)
+        fileList = []
+
+        for i in self.fileList:
+            if i.getFileType() == "d":
+                fileList.append(i.getFileName() + "/")
+            else:
+                fileList.append(i.getFileName())
+
+        if(len(fileList) != 0):
+            sortList = sorted(fileList)
             print("  ".join(sortList))
+        
         return
 
+
 class MyShell:
-    fs = FileSystem()
+    def __init__(self):
+        self.fs = FileSystem()
+        self.wd = "/"   # Workind Directory
     
     def runShell(self):
         istr = InputString()
@@ -55,11 +92,21 @@ class MyShell:
                 i = 1
                 while i < istr.getArgNum():
                     if(self.fs.searchFile(istr.getArg(i)) == False):
-                        self.fs.addFile(istr.getArg(i))
+                        self.fs.addFile(istr.getArg(i), "f", self.wd)
                     i += 1
 
             elif cmd == "ls":
                 self.fs.showFiles()
+
+            elif cmd == "mkdir":
+                i = 1
+                while i < istr.getArgNum():
+                    if(self.fs.searchFile(istr.getArg(i)) == False):
+                        self.fs.addFile(istr.getArg(i), "d", self.wd)
+                    i += 1
+
+            elif cmd == "cd":
+                pass
 
             elif cmd == "exit":
                 break
@@ -68,6 +115,7 @@ class MyShell:
                 print(cmd + ": command not found")
 
             print("")
+
 
 if __name__ == "__main__":
     mysh = MyShell()
